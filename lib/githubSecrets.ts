0105b09ec6c2b897ca0f156f7149fbecfb9004e7
Rohit Gupta <gupta.rohit19@gmail.com>
2022-11-15 10:14:25 +1100

    update github arn

--- lib/githubSecrets.ts
+++ lib/githubSecrets.ts
@@ -4,15 +4,12 @@ import { PolicyStatement, Effect } from '@aws-cdk/aws-iam';
 import * as config from '../environment.json';
 
 export function getSSHKeySecret(scope: Construct, env: string) {
-  let arn = null;
-
-  if (env == 'Testing')
-    arn =
-      `arn:aws:secretsmanager:${config.NonProd_Sydney.region}:${config.NonProd_Sydney.account}:secret:TennisAustraliaBuildToken-oxpHqZ`;
+  let arn = `arn:aws:secretsmanager:${config.NonProd_Sydney.region}:${config.NonProd_Sydney.account}:secret:github-build-token-4j7Cc6`;
 
   if (arn === null) throw new Error('Could not find Github Token for ' + env);
   return secretsmanager.Secret.fromSecretCompleteArn(scope, 'TennisAustraliaBuildToken', arn);
 }
+
 export function getSecretsPolicy() {
   const ssmAccess = new PolicyStatement();
   ssmAccess.effect = Effect.ALLOW;
